5b0cd33ae4091bc1a91d60fe7df343b4d1f497d4
EddiGardi <77970641+EddiGardi@users.noreply.github.com>
2021-02-09 09:08:42 +0100

    Montag

---
+++ DatenbankenZooManagementWPF/DatenbankenZooManagementWPF/MainWindow.xaml.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DatenbankenZooManagementWPF
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection sqlConnection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString = ConfigurationManager.ConnectionStrings["DatenbankenZooManagementWPF.Properties.Settings.FranksDBConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+            ShowZoos();
+            ShowAnimals();
+        }
+
+        public void ShowZoos()
+        {
+            try
+            {
+                string query = "select * from Zoo";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable zooTable = new DataTable();
+                    sqlDataAdapter.Fill(zooTable);
+
+                    // Welche Informationenn der Tabelle in userem DataTable soll in der ListBox angezeigt werden
+                    listZoos.DisplayMemberPath = "Location";
+                    // Welcher Wert soll gegeben werden, wenn eines unsere Items von der Listbox ausgewählt wird.
+                    listZoos.SelectedValuePath = "Id";
+                    //
+                    listZoos.ItemsSource = zooTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        public void ShowAnimals()
+        {
+            try
+            {
+                string query = "select * from Animal";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable animalTable = new DataTable();
+                    sqlDataAdapter.Fill(animalTable);
+
+                    listAnimals.DisplayMemberPath = "Name";
+                    listAnimals.SelectedValuePath = "Id";
+                    listAnimals.ItemsSource = animalTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }    
+        }
+
+        public void ShowPresentAnimals()
+        {
+            if (listZoos.SelectedValue == null)
+            {
+                listPresentAnimals.ItemsSource = null;
+                return;
+            }
+
+            try
+            {
+                string query = "select * from Animal a inner join ZooAnimal za on a.Id = za.AnimalId where za.ZooId = @ZooId";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)
+                {
+                    sqlCommand.Parameters.AddWithValue("@ZooId", listZoos.SelectedValue);
+
+                    DataTable presentAnimalsTable = new DataTable();
+                    sqlDataAdapter.Fill(presentAnimalsTable);
+
+                    // Welche Informationenn der Tabelle in userem DataTable soll in der ListBox angezeigt werden
+                    listPresentAnimals.DisplayMemberPath = "Name";
+                    // Welcher Wert soll gegeben werden, wenn eines unsere Items von der Listbox ausgewählt wird.
+                    listPresentAnimals.SelectedValuePath = "Id";
+                    //
+                    listPresentAnimals.ItemsSource = presentAnimalsTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("b");
+            }
+        }
+
+        private void listZoos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowPresentAnimals();
+            ShowSelectedZooInTextBox();
+        }
+
+        private void listAnimals_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowSelectedAnimalInTextBox();
+        }
+        
+        private void ShowSelectedZooInTextBox()
+        {
+            if (listZoos.SelectedValue == null) return;
+
+            try
+            {
+                string query = "select location from Zoo where Id = @ZooId";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)
+                {
+                    sqlCommand.Parameters.AddWithValue("@ZooId", listZoos.SelectedValue);
+                    DataTable zooDataTable = new DataTable();
+                    sqlDataAdapter.Fill(zooDataTable);
+
+                    tb.Text = zooDataTable.Rows[0]["Location"].ToString();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Test");
+            }
+        }
+
+        private void ShowSelectedAnimalInTextBox()
+        {
+            if (listAnimals.SelectedValue == null) return;
+
+            try
+            {
+                string query = "select name from Animal where Id = @AnimalId";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)
+                {
+                    sqlCommand.Parameters.AddWithValue("@AnimalId", listAnimals.SelectedValue);
+                    DataTable animalDataTable = new DataTable();
+                    sqlDataAdapter.Fill(animalDataTable);
+
+                    tb.Text = animalDataTable.Rows[0]["Name"].ToString();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Test");
+            }
+        }
+
+        private void DeleteZoo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "delete from Zoo where id = @ZooId";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@ZooId", listZoos.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowZoos();
+            }
+        }
+
+        private void AddZoo_Click (object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "insert into Zoo values (@Location)";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Location", tb.Text);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fehler beim Hinzufügen eines Tieres zu einem Zoo.");
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowPresentAnimals();
+            }
+        }
+
+        private void AddAnimal_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "insert into Animal values (@Name)";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Name", tb.Text);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fehler beim Hinzufügen eines Tieres.");
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowAnimals();
+            }
+        }
+
+        private void DeleteAnimal_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "delete from Animal where id = @AnimalId";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@AnimalId", listAnimals.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                ShowAnimals();
+                ShowPresentAnimals();
+                sqlConnection.Close();
+            }
+        }
+
+        private void AddAnimalToZoo_Click(object sender, RoutedEventArgs e)
+        {
+            if (listZoos.SelectedValue == null || listAnimals.SelectedValue == null)
+            {
+                return;
+            }
+            try
+            {
+                string query = "insert into ZooAnimal values (@ZooId, @AnimalId)";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@ZooId", listZoos.SelectedValue);
+                sqlCommand.Parameters.AddWithValue("@AnimalId", listAnimals.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fehler beim hinzufügen");
+            }
+            finally
+            {
+                ShowPresentAnimals();
+                sqlConnection.Close();
+            }
+        }
+
+        private void DeleteAnimalfromZoo_Click(object sender, RoutedEventArgs e)
+        {
+            if (listZoos.SelectedValue == null || listPresentAnimals.SelectedValue == null)
+            {
+                return;
+            }
+            try
+            {
+                string query = "delete from ZooAnimal where ZooId = @ZooId and AnimalId = @AnimalId";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@ZooId", listZoos.SelectedValue);
+                sqlCommand.Parameters.AddWithValue("@AnimalId", listPresentAnimals.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Tier konnte nicht aus dem Zoo entfernt werden.");
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowPresentAnimals();
+            }
+        }
+
+        private void UpdateZoo_Click(object sender, RoutedEventArgs e)
+        {
+            if (listZoos.SelectedValue == null ) return;
+            
+            try
+            {
+                string query = "update Zoo set Location = @Location where Id = @ZooId";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@ZooId", listZoos.SelectedValue);
+                sqlCommand.Parameters.AddWithValue("@Location", tb.Text);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fehler beim Aktualisieren eines Zoos");
+            }
+            finally
+            {
+                ShowZoos();
+                sqlConnection.Close();
+            }
+        }
+
+        private void UpdateAnimal_Click(object sender, RoutedEventArgs e)
+        {
+            if (listAnimals.SelectedValue == null)
+            {
+                return;
+            }
+            try
+            {
+                string query = "update Animal set Name = @Name where Id = @AnimalId";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Name", tb.Text);
+                sqlCommand.Parameters.AddWithValue("@AnimalId", listAnimals.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fehler beim Aktualiseiren eines Tiers");
+            }
+            finally
+            {
+                ShowAnimals();
+                sqlConnection.Close();
+            }
+        }
+    }
+}
---
+++ StudentManagerLinq/StudentManagerLinq/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagerLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniversityManager um = new UniversityManager();
+            um.MaleStudents();
+            um.FemaleStudents();
+            um.SortStudentsByAge();
+            um.AllStudentsFromHSU();
+            um.StudentsFromUniByUniId();
+            um.StudentAndUniversityNameCollection();
+
+            int[] someInts = { 30, 12, 4, 3, 12 };
+            // Sotieren und umderehen
+            IEnumerable<int> sortedInts = from i in someInts orderby i select i;
+            IEnumerable<int> reversedInts = sortedInts.Reverse();
+            // direkt rückwarts sortiert
+            IEnumerable<int> reversedSortedInts = from i in someInts orderby i descending select i;
+
+            Console.ReadKey();
+        }
+    }
+
+    class UniversityManager
+    {
+        public List<University> universities;
+        public List<Student> students;
+
+        // Konstruktor
+        public UniversityManager()
+        {
+            //Initialisieren
+            universities = new List<University>();
+            students = new List<Student>();
+
+            //Befüllen der Universitätslisten
+            universities.Add(new University { Id = 1, Name = "HSU" });
+            universities.Add(new University { Id = 2, Name = "UniBwM" });
+
+            //Befüllen der Studententliste
+            students.Add(new Student { Id = 1, Name = "Chris Renkes", Gender = "Male", Age = 26, UniversityId = 2 });
+            students.Add(new Student { Id = 2, Name = "David Kramer", Gender = "Male", Age = 26, UniversityId = 1 });
+            students.Add(new Student { Id = 3, Name = "Toni", Gender = "Male", Age = 19, UniversityId = 1 });
+            students.Add(new Student { Id = 4, Name = "Carla", Gender = "Female", Age = 23, UniversityId = 2 });
+            students.Add(new Student { Id = 5, Name = "Nina", Gender = "Female", Age = 22, UniversityId = 1 });
+            students.Add(new Student { Id = 5, Name = "Michael", Gender = "Male", Age = 21, UniversityId = 2 });
+        }
+
+        public void SortStudentsByAge()
+        {
+            var sortedStudents = from student in students orderby student.Age select student;
+            Console.WriteLine("Studenten sortiert nach Alter");
+            foreach (var item in students)
+            {
+                item.Print();
+            }
+        }
+
+        public void MaleStudents ()
+        {
+            IEnumerable<Student> maleStudents = from student in students where student.Gender == "Male" select student;
+            Console.WriteLine("Männliche Studenten");
+            foreach (Student item in maleStudents)
+            {
+                item.Print();
+            }
+        }
+
+        public void FemaleStudents ()
+        {
+            IEnumerable<Student> femaleStudents = from student in students where student.Gender == "Female" select student;
+            Console.WriteLine("Weibliche Studenten");
+            foreach (Student item in femaleStudents)
+            {
+                item.Print();
+            }
+        }
+
+        public void AllStudentsFromHSU ()
+        {
+            IEnumerable<Student> hsuStudents = from student in students
+                                               join university in universities on student.UniversityId
+                                               equals university.Id
+                                               where university.Name == "HSU"
+                                               select student;
+            Console.WriteLine("Studenten der HSU:");
+            foreach (var item in hsuStudents)
+            {
+                item.Print();
+            }
+        }
+
+        public void StudentsFromUniByUniId ()
+        {
+            Console.WriteLine("Wie lautet die Id der Universität?");
+            try
+            {
+                int writtenID = Convert.ToInt32(Console.ReadLine());
+
+                IEnumerable<Student> studentsByUniId = from student in students
+                                                       join university in universities on student.UniversityId
+                                                       equals university.Id
+                                                       where university.Id == writtenID
+                                                       select student;
+                Console.WriteLine("Stundenten an der gewählten Uni:");
+                foreach (var item in studentsByUniId)
+                {
+                    item.Print();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Falscher Wert");
+            }
+        }
+
+        public void StudentAndUniversityNameCollection()
+        {
+            var newCollection = from student in students
+                                join university in universities on student.UniversityId
+                                equals university.Id
+                                orderby student.Name
+                                select new { StudentName = student.Name, UniversityName = university.Name };
+            Console.WriteLine("Neue Sammlung");
+            foreach (var item in newCollection)
+            {
+                Console.WriteLine("Student {0} von der {1}", item.StudentName, item.UniversityName);
+            }
+        }
+    }
+
+    class University
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Universität {0} mit der Id {1}.", Name, Id);
+        }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        // Foreign Key = Fremdschlüssel
+        public int UniversityId { get; set; }
+
+        public void Print ()
+        {
+            Console.WriteLine("Student {0} mit der Id {1}, dem Geschlecht {2} " +
+                "und Alter {3} von der Universität mit der Id {4}", Name, Id, Gender, Age, UniversityId);
+        }
+    }
+
+}
